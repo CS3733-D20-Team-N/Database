77977529090152e47ae48f29199c5cbec43da81a
kofichtner <kofichtner@wpi.edu>
2020-03-29 16:32:43 -0400

    adds comment for Kaitlyn's branch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello World!");
     System.out.println("Kaitlyn Fichtner");
+    //comment for Kaitlyn's branch
     System.out.println("Tian Yu Fan");
   }
 }
