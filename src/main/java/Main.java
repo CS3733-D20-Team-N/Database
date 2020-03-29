c656eeb72072f15eceb8acbae032ccadc10c13e8
Nicholas Wood <omfgnickybots@gmail.com>
2020-03-29 16:34:33 -0400

    merge

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,8 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello World!");
     System.out.println("Kaitlyn Fichtner");
-<<<<<<< HEAD
     System.out.println("Nick Wood");
-=======
     System.out.println("Tian Yu Fan");
->>>>>>> 414f0212522a6d041a350079eb139978cc8c7d1e
+
   }
 }
