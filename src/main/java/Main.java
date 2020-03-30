38dd3728956ee447406823d46ebd0ec1d92bdb30
Ivan <ieroshenko@wpi.edu>
2020-03-30 16:42:04 -0400

    lol

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,23 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
+        // names
+
+        System.out.println("Kaitlyn Fichtner");
+        System.out.println("Noah Capucilli-Shatan");
+        System.out.println("Tian Yu Fan");
+        System.out.println("Ivan Eroshenko");
+        System.out.println("Seungho Lee");
+        System.out.println("Nicholas Wood");
+        System.out.println("Matt Johannesen");
+        System.out.println("Evan Llewellyn");
+        System.out.println("Nick Pelletier");
+        System.out.println("Michael Laks");
+        System.out.println("Branch - Annie");
+        System.out.println("Kaitlyn Branch");
+        
+        // branches
+        System.out.println("Branch - Ivan Eroshenko");
   }
 
 }
