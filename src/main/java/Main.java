935b3b7a8d962445b70502787bf2bc34656d0c96
kofichtner <kofichtner@wpi.edu>
2020-03-30 10:03:57 -0400

    adds print statement for kaitlyn branch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello World!");
     System.out.println("Kaitlyn Fichtner");
-    //comment for Kaitlyn's branch
+    System.out.println("Kaitlyn Branch");
     System.out.println("Tian Yu Fan");
   }
 }
