d6bf76de8f9fe25f10b6095467af700c2f3461d4
Nicholas Wood <omfgnickybots@gmail.com>
2020-03-31 15:06:00 -0400

    Checks if I contribute to repository

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,6 +17,7 @@ public class Main {
         System.out.println("Michael Laks");
         System.out.println("Branch - Annie");
         System.out.println("Kaitlyn Branch");
+        System.out.println("Nicholas Wood - Commiter?");
         
         // branches
         System.out.println("Branch - Ivan Eroshenko");
